8f0817648db1c247d4ddd13399adafaa413940f0
Jacob Tillotson <jacobtill@live.com>
2024-01-23 20:51:28 +0000

    Make telescope grep and indentations better

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -11,7 +11,5 @@ require('telescope').setup{
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fg', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
