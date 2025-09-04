67e21d4808c6f4d9c9d19aa0f942db3f70391999
Jacob Tillotson <jacobtill@live.com>
2025-09-04 10:28:47 +0100

    Make lsp settings behave better across machines

--- lua/jist/lsp.lua
+++ lua/jist/lsp.lua
@@ -1,25 +1,5 @@
 -- Individual LSP setup
---vim.lsp.enable('luals')
-require'lspconfig'.lua_ls.setup{}
-require'lspconfig'.clangd.setup{}
-require'lspconfig'.ols.setup{
-    init_options = {
-        enable_fake_methods = true,
-    }
-}
-require'lspconfig'.jedi_language_server.setup{}
-require'lspconfig'.rust_analyzer.setup{}
-
--- General LSP config
--- A bunch of dead stuff since im using blink instead
---vim.api.nvim_create_autocmd('LspAttach', {
---  callback = function(ev)
---    local client = vim.lsp.get_client_by_id(ev.data.client_id)
---    if client:supports_method('textDocument/completion') then
---      vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = true})
---    end
---  end,
---})
---vim.opt.completeopt = {'noinsert', 'menu', 'menuone', 'popup', 'fuzzy', 'noselect', 'preview'}
-
-vim.diagnostic.config({virtual_text = true})
+-- simply download the LSP via the package manager (homebrew or similar)
+-- and do `vim.lsp.enable('lsp_name')
+--
+-- For example to enable lua_ls do `vim.lsp.enable('lua_ls')`
